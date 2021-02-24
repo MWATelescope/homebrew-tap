113474abffa843d0bb152f3e8f44c3c741f95694
Derwent McElhinney <derwentx@gmail.com>
2021-02-24 20:03:59 +0800

    🚨 fix brew audit issues

--- aoflagger.rb
+++ aoflagger.rb
@@ -1,29 +1,29 @@
 class Aoflagger < Formula
-  desc "Find and remove radio-frequency interference (RFI) in radio astronomical observations"
+  desc "Find and remove radio-frequency interference in radio astronomical observations"
   homepage "https://gitlab.com/aroffringa/aoflagger"
+  license "GPL-3.0-only"
 
-  url "https://gitlab.com/aroffringa/aoflagger.git", :using => :git, :tag => "v3.0.0"
+  stable do
+    url "https://gitlab.com/aroffringa/aoflagger.git", tag: "v3.0.0", revision: "8eda93ed6344d99698dc925264e649e6340f66ba"
+    patch :DATA
+  end
 
-  depends_on "ska-sa/tap/casacore"
-  depends_on "lua@5.3"
-  depends_on "fftw"
-  depends_on "boost"
-  depends_on "boost-build"
-  depends_on "lapack"
-  depends_on "cfitsio"
-  depends_on "libpng"
-  depends_on "python@3.9"
-  depends_on "pybind11"
+  head do
+    url "https://gitlab.com/aroffringa/aoflagger.git"
+  end
 
   depends_on "cmake" => :build
 
-  head do
-    url "https://gitlab.com/aroffringa/aoflagger.git", :using => :git
-  end
-
-  stable do
-    patch :DATA
-  end
+  depends_on "boost"
+  depends_on "boost-build"
+  depends_on "cfitsio"
+  depends_on "fftw"
+  depends_on "lapack"
+  depends_on "libpng"
+  depends_on "lua@5.3"
+  depends_on "pybind11"
+  depends_on "python@3.9"
+  depends_on "ska-sa/tap/casacore"
 
   def install
     build_type = "Release"
@@ -32,9 +32,12 @@ class Aoflagger < Formula
       cmake_args.delete "-DCMAKE_BUILD_TYPE=None"
       cmake_args << "-DCMAKE_BUILD_TYPE=#{build_type}"
       system "cmake", "../..", *cmake_args
-      system "cat", "Makefile"
       system "make", "install"
     end
+  end
+
+  test do
+    File.exist? include / "aoflagger.h"
   end
 end
 
@@ -44,11 +47,11 @@ index 957cb76..0916bb7 100644
 --- a/CMakeLists.txt
 +++ b/CMakeLists.txt
 @@ -77,8 +77,6 @@ endif("${isSystemDir}" STREQUAL "-1")
- 
+
  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror=vla -DNDEBUG -funroll-loops -O3 -std=c++11")
- 
+
 -string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,--no-undefined")
 -
  include(CheckCXXCompilerFlag)
- include(CheckSymbolExists) 
+ include(CheckSymbolExists)
  include(CheckCXXSymbolExists)
