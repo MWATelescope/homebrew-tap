a5e2a772c8a25636d0d7a48e39caa33ab356becf
Derwent McElhinney <derwentx@gmail.com>
2022-05-23 16:11:26 +0800

    aoflagger 3.1.0

--- Formula/aoflagger.rb
+++ Formula/aoflagger.rb
@@ -4,8 +4,7 @@ class Aoflagger < Formula
   license "GPL-3.0-only"
 
   stable do
-    url "https://gitlab.com/aroffringa/aoflagger.git", tag: "v3.0.0", revision: "8eda93ed6344d99698dc925264e649e6340f66ba"
-    patch :DATA
+    url "https://gitlab.com/aroffringa/aoflagger.git", tag: "v3.1.0", revision: "18b70b9836552d7a632c457ffd8822e57a3ebe7b"
   end
 
   head do
@@ -40,18 +39,3 @@ class Aoflagger < Formula
     File.exist? include / "aoflagger.h"
   end
 end
-
-__END__
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 957cb76..0916bb7 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -77,8 +77,6 @@ endif("${isSystemDir}" STREQUAL "-1")
-
- set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Werror=vla -DNDEBUG -funroll-loops -O3 -std=c++11")
-
--string(APPEND CMAKE_SHARED_LINKER_FLAGS " -Wl,--no-undefined")
--
- include(CheckCXXCompilerFlag)
- include(CheckSymbolExists)
- include(CheckCXXSymbolExists)
